e72f83896065ef7dd10d86dd4d1842161e7cae6b
Hugo Tavares <hm533@cam.ac.uk>
2026-06-12 14:21:24 +0100

    add  to relevant chunks; update purl script

--- utils/purl_materials.sh
+++ utils/purl_materials.sh
@@ -11,6 +11,13 @@ mkdir -p course_files/scripts
 
 # extract R code from all .qmd files in the materials directory and save to course_files/scripts
 for f in materials/*.qmd; do
+    # if file does not contain `{r}` chunks, skip it
+    if ! grep -q "{r" "$f"; then
+        echo "Skipping $f (no R chunks found)"
+        continue
+    fi
+
+    # otherwise purl
     out="course_files/scripts/$(basename "${f%.qmd}.R")"
 
     Rscript -e "
